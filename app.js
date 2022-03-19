f6a2ae35ab9d6849f9ccad4c6fc49f4e3a5300ac
Jincy Varghese <jincyvar@gmail.com>
2022-03-19 22:06:40 +0530

    app update

--- app.js
+++ app.js
@@ -39,4 +39,7 @@ app.get('/', function (req, res) {
     });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our Library app is running on port ${ PORT }`);
+});
