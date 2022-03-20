2a53cf7aadbeac1b361bebbff26415787b34486c
Jincy Varghese <jincyvar@gmail.com>
2022-03-20 10:40:02 +0530

    js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const authorRouter = require('./src/routes/authorRoutes')(nav)
 const booksRouter = require('./src/routes/bookroutes')(nav)
 const signRouter = require('./src/routes/signuproutes')
 const loginRouter = require('./src/routes/loginroutes')
-const addbookRouter = require('./src/routes/addbookroutes')(nav)
+const addbookRouter = require('./src/routes/addbookRoutes')(nav)
 const addauthorRouter = require('./src/routes/addauthorroutes')(nav)
 const homeRouter = require('./src/routes/homeroutes')(nav)
 
